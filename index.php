c1dd9503eef99c2590db32d3ece85a523fde383b
tremogliec <tremogliec@gmail.com>
2015-02-11 20:45:32 -0500

    build 1.05

--- index.php
+++ index.php
@@ -5,93 +5,113 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width">
-        <meta name="format-detection" content="telephone=no">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    <meta name="format-detection" content="telephone=no">
 
-        <link rel="stylesheet" href="css/normalize.min.css">
-        <link rel="stylesheet" href="css/main.css">
-        <link href='http://fonts.googleapis.com/css?family=Patua+One|Roboto' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/normalize.min.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link href='http://fonts.googleapis.com/css?family=Patua+One|Roboto' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/font-awesome.min.css">
 
-        <!--[if lt IE 9]>
-            <script src="js/vendor/html5-3.6-respond-1.1.0.min.js"></script>
-        <![endif]-->
+<!--[if lt IE 9]>
+<script src="js/vendor/html5-3.6-respond-1.1.0.min.js"></script>
+<![endif]-->
 
-        <!--Google Analytics-->
+<!--Google Analytics-->
 
-        <!---->
-    </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-        <![endif]-->
+<!---->
+</head>
+<body>
+<!--[if lt IE 7]>
+<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+<![endif]-->
 
-        <div class="header">
-            <div class="container">
-                <nav>
-                    <a href="/">Home</a>
-                    <a href="/">Shop</a>
-                    <a href="/">Gallery</a>
-                    <a href="/">About</a>
-                    <a href="/">Contact</a>
-                    <span>
-                    <a href="/">My Account</a>
-                    <a href="/"><span class="fa fa-shopping-cart fa-lg"></span></a>
-                    </span>
-                </nav>
+<div class="header">
+    <div class="container">
+        <nav>
+            <a href="/">Home</a>
+            <a href="/">Shop</a>
+            <a href="/">Gallery</a>
+            <a href="/">About</a>
+            <a href="/">Contact</a>
+            <span>
+                <a href="/">My Account</a>
+                <a href="/"><span class="fa fa-shopping-cart fa-lg"></span></a>
+            </span>
+        </nav>
 
-                <div class="logo"><img src="images/GoodVibes-Logo.svg" alt="Good Vibes Intercourse PA"></div>
-                <div class="brands cf">
-                    <div class="grid-1"><a href=""><img src="images/logos/lazy-one-logo.png"></a></div>
-                    <div class="grid-1"><a href=""><img src="images/logos/good-vibes-logo.png"></a></div>
-                    <div class="grid-1"><a href=""><img src="images/logos/natural-life-logo.png"></a></div>
-                    <div class="grid-1"><a href=""><img src="images/logos/old-guys-rule-logo.png"></a></div>
-                    <div class="grid-1"><a href=""><img src="images/logos/laid-back-logo.png"></a></div>
-                </div>
+        <div class="logo"><img src="images/GoodVibes-Logo.svg" alt="Good Vibes Intercourse PA"></div>
+        <div class="brands cf">
+            <div class="grid-1"><a href=""><img src="images/logos/lazy-one-logo.png"></a></div>
+            <div class="grid-1"><a href=""><img src="images/logos/good-vibes-logo.png"></a></div>
+            <div class="grid-1"><a href=""><img src="images/logos/natural-life-logo.png"></a></div>
+            <div class="grid-1"><a href=""><img src="images/logos/old-guys-rule-logo.png"></a></div>
+            <div class="grid-1"><a href=""><img src="images/logos/laid-back-logo.png"></a></div>
+        </div>
+    </div>
+</div>
+
+<div class="home_page-content page-content center">
+    <div class="container">
+        <h1>Spread Good Vibes</h1>
+        <p>Stop by our shop and spread the good vibes! You will find a great selection of fun and comfortable attire.</p>
+        <p>Also available In Store - <span>Life is Good</span> and <span>Hatley!</span></p>
+        <div class="home_product-grid">
+            <div class="box-large"></div>
+            <div class="box-container">
+            <div class="box-small small-top"></div>
+                <div class="box-medium"></div>
+            </div>
+            <div class="box-large"></div>
+            <div class="box-container">
+                <div class="box-medium"></div>
+                <div class="box-small small-bottom"></div>
             </div>
         </div>
+    </div>
+</div>
 
-        <div>
-            <div class="test grid-1"><p>Test</p></div>
-            <div class="test grid-1"></div>
-            <div class="test grid-1"></div>
-            <div class="test grid-1"></div>
-            <div class="test grid-1"></div>
-            <div class="test grid-1"></div>
-        </div>
-        <div>
-            <div class="test grid-2"></div>
-            <div class="test grid-2"></div>
-            <div class="test grid-2"></div>
-        </div>
-        <div>
-            <div class="test grid-3"></div>
-            <div class="test grid-3"></div>
-        </div>
-        <div>
-            <div class="test grid-4"></div>
-            <div class="test grid-2"></div>
-        </div>
-        <div>
-            <div class="test grid-full"></div>
-        </div>
-        <div>
-            <div class="test grid-25"></div>
-            <div class="test grid-25"></div>
-            <div class="test grid-25"></div>
-            <div class="test grid-25"></div>
-        </div>
+<div>
+    <div class="test grid-1"><p>Test</p></div>
+    <div class="test grid-1"></div>
+    <div class="test grid-1"></div>
+    <div class="test grid-1"></div>
+    <div class="test grid-1"></div>
+    <div class="test grid-1"></div>
+</div>
+<div>
+    <div class="test grid-2"></div>
+    <div class="test grid-2"></div>
+    <div class="test grid-2"></div>
+</div>
+<div>
+    <div class="test grid-3"></div>
+    <div class="test grid-3"></div>
+</div>
+<div>
+    <div class="test grid-4"></div>
+    <div class="test grid-2"></div>
+</div>
+<div>
+    <div class="test grid-full"></div>
+</div>
+<div>
+    <div class="test grid-25"></div>
+    <div class="test grid-25"></div>
+    <div class="test grid-25"></div>
+    <div class="test grid-25"></div>
+</div>
 
 
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="bower_components/jquery/dist/jquery.min.js"><\/script>')</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="bower_components/jquery/dist/jquery.min.js"><\/script>')</script>
 
-        <script src="js/main.js"></script>
-    </body>
+<script src="js/main.js"></script>
+</body>
 </html>
 <?php include 'includes/footer.php';?>
